0346e2572b6c8afa16fc2793b9393d8e9f0c7e2a
Ubuntu <ubuntu@ip-172-31-43-94.eu-central-1.compute.internal>
2018-10-16 07:11:18 +0000

    Init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://3.120.232.194/admin/');
+define('HTTP_CATALOG', 'http://3.120.232.194/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://3.120.232.194/admin/');
+define('HTTPS_CATALOG', 'http://3.120.232.194/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ubuntu/www/opencart2/admin/');
+define('DIR_SYSTEM', '/home/ubuntu/www/opencart2/system/');
+define('DIR_LANGUAGE', '/home/ubuntu/www/opencart2/admin/language/');
+define('DIR_TEMPLATE', '/home/ubuntu/www/opencart2/admin/view/template/');
+define('DIR_CONFIG', '/home/ubuntu/www/opencart2/system/config/');
+define('DIR_IMAGE', '/home/ubuntu/www/opencart2/image/');
+define('DIR_CACHE', '/home/ubuntu/www/opencart2/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/ubuntu/www/opencart2/system/storage/download/');
+define('DIR_LOGS', '/home/ubuntu/www/opencart2/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/ubuntu/www/opencart2/system/storage/modification/');
+define('DIR_UPLOAD', '/home/ubuntu/www/opencart2/system/storage/upload/');
+define('DIR_CATALOG', '/home/ubuntu/www/opencart2/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart2');
+define('DB_PASSWORD', 'opencart2');
+define('DB_DATABASE', 'opencart2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://3.120.232.194/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://3.120.232.194/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ubuntu/www/opencart2/catalog/');
+define('DIR_SYSTEM', '/home/ubuntu/www/opencart2/system/');
+define('DIR_LANGUAGE', '/home/ubuntu/www/opencart2/catalog/language/');
+define('DIR_TEMPLATE', '/home/ubuntu/www/opencart2/catalog/view/theme/');
+define('DIR_CONFIG', '/home/ubuntu/www/opencart2/system/config/');
+define('DIR_IMAGE', '/home/ubuntu/www/opencart2/image/');
+define('DIR_CACHE', '/home/ubuntu/www/opencart2/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/ubuntu/www/opencart2/system/storage/download/');
+define('DIR_LOGS', '/home/ubuntu/www/opencart2/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/ubuntu/www/opencart2/system/storage/modification/');
+define('DIR_UPLOAD', '/home/ubuntu/www/opencart2/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart2');
+define('DB_PASSWORD', 'opencart2');
+define('DB_DATABASE', 'opencart2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
